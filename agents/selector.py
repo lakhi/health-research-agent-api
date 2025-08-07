207fd66710c6406c0998c5fb33c90e60b7755caf
Akshay Lakhi <akshay.lakhi@mindfort.tech>
2025-08-07 15:46:44 +0200

    upgraded agno and other project dependencies + added health research network agent in the selector + wip: Azure Open AI working for hrn agent

--- agents/selector.py
+++ agents/selector.py
@@ -2,9 +2,11 @@ from enum import Enum
 from typing import List, Optional
 
 from agents.marhonivirus_agent import get_marhinovirus_agent
+from agents.health_research_network_agent import get_health_research_network_agent
 
 
 class AgentType(Enum):
+    HEALTH_RESEARCH_NETWORK_AGENT = "hrn_agent"
     MARHONIVIRUS_AGENT = "marhonivirus_agent"
 
 
@@ -14,7 +16,7 @@ def get_available_agents() -> List[str]:
 
 
 def get_agent(
-    model_id: str = "gpt-4.1",
+    model_id: str = "gpt-4o",
     agent_id: Optional[AgentType] = None,
     user_id: Optional[str] = None,
     session_id: Optional[str] = None,
@@ -27,6 +29,12 @@ def get_agent(
             session_id=session_id,
             debug_mode=debug_mode,
         )
-    # elif agent_id == AgentType.AGNO_debug_mode)
+    elif agent_id == AgentType.HEALTH_RESEARCH_NETWORK_AGENT:
+        return get_health_research_network_agent(
+            model_id=model_id,
+            user_id=user_id,
+            session_id=session_id,
+            debug_mode=debug_mode,
+        )
 
     raise ValueError(f"Agent: {agent_id} not found")
--- api/routes/playground.py
+++ api/routes/playground.py
@@ -1,6 +1,8 @@
 from agno.playground import Playground
 
+from agents.health_research_network_agent import get_health_research_network_agent
 from agents.marhonivirus_agent import get_marhinovirus_agent
+
 ######################################################
 
 ## Routes for the Playground Interface
@@ -9,9 +11,10 @@ from agents.marhonivirus_agent import get_marhinovirus_agent
 # Get Agents to serve in the playground
 
 marhonivirus_agent = get_marhinovirus_agent(debug_mode=True)
+hrn_agent = get_health_research_network_agent()
 
 # Create a playground instance
-playground = Playground(agents=[marhonivirus_agent])
+playground = Playground(agents=[hrn_agent, marhonivirus_agent])
 
 # Get the router for the playground
 playground_router = playground.get_async_router()
