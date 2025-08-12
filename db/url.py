55b64a44362dbcbb531bf3eee0e0bd069d2d55fa
Akshay Lakhi <akshay.lakhi@mindfort.tech>
2025-08-12 17:35:38 +0200

    upgraded to psycopg2 since Azure requires it for successful connection + added test azure connection which works!

--- db/url.py
+++ db/url.py
@@ -1,19 +1,21 @@
 from os import getenv
 
-
 def get_db_url() -> str:
     # Standard connection (for local development)
-    db_driver = getenv("DB_DRIVER", "postgresql+psycopg")
+    db_driver = getenv("DB_DRIVER", "postgresql+psycopg2")
     db_user = getenv("DB_USER")
     db_pass = getenv("DB_PASS")
     db_host = getenv("DB_HOST")
     db_port = getenv("DB_PORT")
     db_database = getenv("DB_DATABASE")
-    return "{}://{}{}@{}:{}/{}".format(
-        db_driver,
-        db_user,
-        f":{db_pass}" if db_pass else "",
-        db_host,
-        db_port,
-        db_database,
-    )
+
+    base_url = f"{db_driver}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_database}"
+
+    # if "azure" in db_host:
+    #     # Azure PostgreSQL connection
+    #     db_driver = "postgresql+psycopg2"
+    #     ssl_mode = "?sslmode=require"
+
+    #     base_url
+
+    return base_url
