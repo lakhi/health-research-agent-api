b770b646db94fa4231734355dc25ca6b7ebd02ec
Akshay Lakhi <akshay.lakhi@mindfort.tech>
2025-08-25 14:15:25 +0200

    generate separate requirements file for linux platform as CPU-only torch dependency was required for Azure. corresponding changes in the azure dockerfile

--- scripts/generate_requirements.sh
+++ scripts/generate_requirements.sh
@@ -13,13 +13,22 @@ source ${CURR_DIR}/_utils.sh
 
 print_heading "Generating requirements.txt..."
 
-if [[ "$#" -eq 1 ]] && [[ "$1" = "upgrade" ]];
-then
-  print_heading "Generating requirements.txt with upgrade"
+if [[ "$1" = "linux" ]]; then
+  print_heading "Generating requirements.txt for Linux deployment..."
+  UV_CUSTOM_COMPILE_COMMAND="./scripts/generate_requirements.sh linux" \
+    uv pip compile ${REPO_ROOT}/pyproject.toml \
+    --index-strategy unsafe-best-match \
+    --python-platform x86_64-manylinux_2_28 \
+    --python-version 3.12 \
+    --no-cache --upgrade -o ${REPO_ROOT}/requirements-linux.txt
+elif [[ "$1" = "upgrade" ]]; then
+  print_heading "Generating requirements.txt for local development..."
   UV_CUSTOM_COMPILE_COMMAND="./scripts/generate_requirements.sh upgrade" \
-    uv pip compile ${REPO_ROOT}/pyproject.toml --no-cache --upgrade -o ${REPO_ROOT}/requirements.txt
+    uv pip compile ${REPO_ROOT}/pyproject.toml \
+    --no-cache --upgrade -o ${REPO_ROOT}/requirements.txt
 else
-  print_heading "Generating requirements.txt"
+  print_heading "Generating requirements.txt for local development..."
   UV_CUSTOM_COMPILE_COMMAND="./scripts/generate_requirements.sh" \
-    uv pip compile ${REPO_ROOT}/pyproject.toml --no-cache -o ${REPO_ROOT}/requirements.txt
+    uv pip compile ${REPO_ROOT}/pyproject.toml \
+    --no-cache -o ${REPO_ROOT}/requirements.txt
 fi
