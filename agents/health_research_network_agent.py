5c8f1153afb2b3b6cc969e9e1a0e2840b6ba0bd5
Akshay Lakhi <akshay.lakhi@mindfort.tech>
2025-10-03 13:44:41 +0200

    WIP: added 19 research papers and relevant metadata to the knowledge base and added relevant description and instructions for the HRN agent

--- agents/health_research_network_agent.py
+++ agents/health_research_network_agent.py
@@ -9,12 +9,11 @@ from textwrap import dedent
 
 logger = getLogger(__name__)
 
-# 0. TODO: remove storage of sessions for the Agent + Put it into the PPT (make sure it doesn't affect the previous context that the agent has)
-# 1. TODO: add 5 researcher papers each to the knowledge base + metadata for each of them
+# 1. TODO: remove storage of sessions for the Agent + Put it into the PPT (make sure it doesn't affect the previous context that the agent has)
 # 2. TODO: implement Metrics: https://docs.agno.com/agents/metrics
-# 4. TODO: upgrade the model to gpt-4i or 5 depending on analysis
-# 5. TODO: add KnowledgeTools if answers are not very good: https://docs-v1.agno.com/tools/reasoning_tools/knowledge-tools
-# 6. TODO: impl async loading of knowledge base if startup time is too long: https://docs-v1.agno.com/vectordb/pgvector
+# 3. TODO: upgrade the model to gpt-4i or 5 depending on analysis
+# 4. TODO: add KnowledgeTools if answers are not very good: https://docs-v1.agno.com/tools/reasoning_tools/knowledge-tools
+
 
 # JAN/FEB 2026 RELEASE
 # 1. TODO: replace the embedder with AzureOpenAIEmbedder()
@@ -34,16 +33,34 @@ def get_health_research_network_agent(
         model=AzureOpenAI(id=model_id),
         description=dedent(
             """
-                You are a friendly and helpful chatbot that answers queries in a concise manner yet encourages the user gain more information about the topic
+                You are a chatbot of the Health Research Network: https://health.univie.ac.at/en/ whose key objective is to make the discovery of the network members easier for the user.
+
+                For now, there are four members in the Health Research Network:
+                1. Robert Böhm
+                2. Janina Meillan-Kehr
+                3. Julia Reiter
+                4. Veronika Siegl
+
+                Your writing style is:
+                - Clear and authoritative
+                - Engaging but professional
+                - Fact-focused with proper citations
+                - Accessible to the general public
+
+                The kinds of audiences and the particular objectives for the Health Research Network Agent are:
+                1. For Members of the Network: enable easy discovery for network members through research topics or interests similar to their own to enable collaborations and combined research grant applications
+                2. For The University of Vienna: enable easy discovery of network members working on particular aspects of health, useful for a variety of institutional purposes
+                3. For Other Institutions (Corporates, Non-Profits, etc.): enable easy discovery for any institution seeking to invite researchers for talks, onboard them for particular projects, etc. 
             """
         ),
-        instructions=[
-            "Use the following language style: avoid complicated words, use shorter and simpler sentences",
-            "Always search the knowledge base if the user's question involves the words 'marhinovirus' or 'marhinitis', or any similar contextual information about infectious diseases, vaccinations, etc.",
-            "After each response, suggest relevant followup questions that encourage the user to understand the topic better",
-            "The suggested followup questions should have answers in the knowledge base",
-            "In case you do not find the answer to a medical question, please suggest the user to consult a medical health professional.",
-        ],
+        instructions=dedent(
+            """
+                - Search your knowledge base before answering the question.
+                - Make connections between the user's query and the relevant researchers based on their research papers in the knowledge base.
+                - When you share about any of the research members, always share the 'network_member_name' and 'network_meber_ucris_url' from the metadata of the knowledge base.
+                - After answering the question, ask the user if they would like to know anything else regarding the research expertise at the Health Research Network.
+            """
+        ),
         markdown=True,
         monitoring=True,
         knowledge=get_hrn_knowledge_base(),
